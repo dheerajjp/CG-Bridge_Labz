f4d5ef5d0f2e9fff946c8b3b56bcaf324f5e619f
Dheeraj J <dheerajjp002@gmail.com>
2025-12-18 11:56:05 +0530

    Arrays

---
+++ 17-Dec-2025/MainProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter Level - ");
+        int n = int.Parse(Console.ReadLine());
+
+        if(n == 1)
+        {
+
+            Console.WriteLine("Enter choice of program");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+
+                case 1:
+                    Vote.Run();
+                    break;
+
+                case 2:
+                    EvenOdd.Run();
+                    break;
+
+            }
+            
+            }
+    }
+}
